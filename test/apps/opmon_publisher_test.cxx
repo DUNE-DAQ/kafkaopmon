6105c96e61d4ca4132edae0aa7566da5b94acfb8
Marco Roda <mroda@liverpool.ac.uk>
2024-02-28 17:51:18 +0100

    Update publisher test with multi threaded inputs

--- test/apps/opmon_publisher_test.cxx
+++ test/apps/opmon_publisher_test.cxx
@@ -4,6 +4,7 @@
 
 #include <chrono>
 #include <thread>
+#include <future>        
 
 #include <kafkaopmon/OpMonPublisher.hpp>
 #include "opmonlib/Utils.hpp"
@@ -16,6 +17,11 @@ namespace po = boost::program_options;
 
 using namespace dunedaq::kafkaopmon;
 using namespace dunedaq::opmonlib;
+
+
+
+
+
 int
 main(int argc, char const* argv[])
 {
@@ -24,6 +30,7 @@ main(int argc, char const* argv[])
     ("help,h", "produce help message")
     ("bootstrap", po::value<std::string>()->default_value("monkafka.cern.ch:30092"), "kafka bootstrap server")
     ("topic,t", po::value<std::string>(), "Optional specification of a topic" )
+    ("n_threads,n", po::value<unsigned int>()->default_value(10), "Number of threads used for test") 
   ;
 
   po::variables_map input_map;
@@ -43,18 +50,30 @@ main(int argc, char const* argv[])
   }
 
   OpMonPublisher p(conf);
-  
-  for( auto i = 0 ; i < 50; ++i ) {
-    dunedaq::opmon::TestInfo ti;
-    ti.set_int_example( 10*i );
-    ti.set_string_example( "test" );
-    auto e = to_entry( ti );
-    e.set_opmon_id("test.app");
-    p.publish( std::move(e) );
 
-    std::this_thread::sleep_for(std::chrono::milliseconds(5));
+  auto pub_func = [&](int i){
+    auto opmon_id = "test.app.thread_" + std::to_string(i);
+    for (auto j = 0; j < 20; ++j ) {
+      dunedaq::opmon::TestInfo ti;
+      ti.set_int_example( j*1000 + i );
+      ti.set_string_example( "test" );
+      auto e = to_entry( ti );
+      e.set_opmon_id(opmon_id);
+      p.publish( std::move(e) );
+    }
+  };
+
+  auto n = input_map["n_threads"].as<unsigned int>() ;
+  std::vector<std::future<void>> threads(n);
+  
+  for( auto i = 0 ; i < n; ++i ) {
+    threads[i] = async(std::launch::async, pub_func, i);
   }
 
+  for ( auto & t : threads ) {
+    t.get();
+  }
+  
   std::this_thread::sleep_for(std::chrono::seconds(2));
   
   return 0;
