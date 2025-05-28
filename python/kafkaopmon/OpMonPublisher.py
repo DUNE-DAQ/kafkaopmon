7a6ef459b9196911b9532c0f8fbeccb0639fa6b5
PawelPlesniak <pmp115@ic.ac.uk>
2025-05-28 15:51:50 +0200

    Updated imports

--- python/kafkaopmon/OpMonPublisher.py
+++ python/kafkaopmon/OpMonPublisher.py
@@ -7,8 +7,13 @@ from google.protobuf.message import Message as Msg
 from google.protobuf.timestamp_pb2 import Timestamp
 from kafka import KafkaProducer
 from opmonlib.conf import OpMonConf
-from opmonlib.opmon_entry_pb2 import OpMonEntry
-from opmonlib.utils import to_entry
+from opmonlib.utils import (
+    extract_key,
+    extract_topic,
+    log_level_from_str,
+    setup_rich_handler,
+    to_entry,
+)
 
 
 class OpMonPublisher:
@@ -17,45 +22,24 @@ class OpMonPublisher:
     def __init__(self, conf: OpMonConf) -> None:
         """Construct the object to publish OpMon metrics to kafka."""
         self.log = logging.getLogger("OpMonPublisher")
-        self.log.setLevel(conf.level)
         self.conf = conf
+        if isinstance(self.conf.level, str):
+            self.conf.level = log_level_from_str(self.conf.level)
+        self.log.setLevel(self.conf.level)
+        self.log.addHandler(setup_rich_handler())
 
         if self.conf.opmon_type != "stream":
             self.log.error("Type must be stream to publish to kafka.")
             sys.exit(1)
 
-        self.topic = "monitoring." + self.conf.topic
+        self.default_topic = "monitoring." + self.conf.topic
 
-        self.producer = KafkaProducer(
+        self.publisher = KafkaProducer(
             bootstrap_servers=conf.bootstrap,
             value_serializer=lambda v: v.SerializeToString(),
             key_serializer=lambda k: str(k).encode("utf-8"),
         )
         return
-
-    def extract_topic(self, message: Msg) -> str:
-        """Extract the topic intended to publish to."""
-        if not self.producer:
-            self.log.warning(
-                "Producer incorrectly formatted, nothing will be published."
-            )
-            return None
-        return self.default_topic
-
-    def extract_key(self, opmon_entry: OpMonEntry) -> str:
-        """Extract  the key from the OpMonEntry."""
-        if not self.producer:
-            self.log.warning(
-                "Improperly initialized OpMonProducer used, nothing will be published."
-            )
-            return None
-        key = str(opmon_entry.origin.session)
-        if opmon_entry.origin.application != "":
-            key += "." + opmon_entry.origin.application
-        for substructure_id in opmon_entry.origin.substructure:
-            key += "." + substructure_id
-        key += "/" + str(opmon_entry.measurement)
-        return key
 
     def publish(
         self,
@@ -64,13 +48,17 @@ class OpMonPublisher:
         message: Msg,
         custom_origin: dict[str, str] | None = None,
         substructure: list[str] | None = None,
-        level: int | None = None,
+        level: int | str | None = None,
     ) -> None:
         """Send an OpMonEntry to Kafka."""
         if not isinstance(message, Msg):
             self.log.error("Passed message needs to be of type google.protobuf.message")
             return
         if not level:
+            level = self.conf.level
+        if isinstance(level, str):
+            level = log_level_from_str(level)
+        if level < self.conf.level:
             return
         metric = to_entry(
             session=session,
@@ -80,8 +68,8 @@ class OpMonPublisher:
             substructure=substructure,
             t=Timestamp().GetCurrentTime(),
         )
-        target_topic = self.extract_topic(message)
-        target_key = self.extract_key(metric)
+        target_topic = extract_topic(message)
+        target_key = extract_key(metric)
 
-        self.producer.send(target_topic, value=metric, key=target_key)
+        self.publisher.send(target_topic, value=metric, key=target_key)
         return
