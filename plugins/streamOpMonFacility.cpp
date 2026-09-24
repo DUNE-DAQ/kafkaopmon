2725dcfed537ea3f4e0267e9617920de06012144
PawelPlesniak <plesniakpaul@gmail.com>
2026-09-24 18:18:59 +0200

    Clang format and version bump

--- plugins/streamOpMonFacility.cpp
+++ plugins/streamOpMonFacility.cpp
@@ -12,39 +12,38 @@ using json = nlohmann::json;
 
 namespace dunedaq::kafkaopmon { // namespace dunedaq
 
-  streamOpMonFacility::streamOpMonFacility(std::string uri, dunedaq::opmonlib::OptionalOrigin o)
-    : dunedaq::opmonlib::OpMonFacility(uri) {
-    
-    std::regex uri_re(R"(([a-zA-Z]+):\/\/([^:\/?#\s]+):(\d+)\/([^:\/?#\s]+))");
-    //* 1st Capturing Group `([a-zA-Z])`: Matches protocol
-    //* 2nd Capturing Group `([^:\/?#\s])+`: Matches hostname
-    //* 3rd Capturing Group `(\d)`: Matches port
-    //* 4th Capturing Group `([^\/?#]+)?`: Matches kafka topic
+streamOpMonFacility::streamOpMonFacility(std::string uri, dunedaq::opmonlib::OptionalOrigin o)
+  : dunedaq::opmonlib::OpMonFacility(uri)
+{
 
-    std::smatch uri_match;
-    if (!std::regex_match(uri, uri_match, uri_re)) {
-      throw WrongURI(ERS_HERE, uri);
-    }
+  std::regex uri_re(R"(([a-zA-Z]+):\/\/([^:\/?#\s]+):(\d+)\/([^:\/?#\s]+))");
+  //* 1st Capturing Group `([a-zA-Z])`: Matches protocol
+  //* 2nd Capturing Group `([^:\/?#\s])+`: Matches hostname
+  //* 3rd Capturing Group `(\d)`: Matches port
+  //* 4th Capturing Group `([^\/?#]+)?`: Matches kafka topic
 
-    json config;
-    std::string bootstrap = uri_match[2];
-    bootstrap += ':' ;
-    bootstrap += uri_match[3];
-    config["bootstrap"] = bootstrap;
-
-    // optionally set the ID of the application
-    // But really this is temporary, and in the future we should avoid env variables
-    if ( o ) {
-      config["cliend_id"] = dunedaq::opmonlib::to_string( o.value() ) ;
-    }
-
-    config["default_topic"] = uri_match[4];
-
-    m_publisher.reset( new OpMonPublisher(config) );
+  std::smatch uri_match;
+  if (!std::regex_match(uri, uri_match, uri_re)) {
+    throw WrongURI(ERS_HERE, uri);
   }
-  
+
+  json config;
+  std::string bootstrap = uri_match[2];
+  bootstrap += ':';
+  bootstrap += uri_match[3];
+  config["bootstrap"] = bootstrap;
+
+  // optionally set the ID of the application
+  // But really this is temporary, and in the future we should avoid env variables
+  if (o) {
+    config["cliend_id"] = dunedaq::opmonlib::to_string(o.value());
+  }
+
+  config["default_topic"] = uri_match[4];
+
+  m_publisher.reset(new OpMonPublisher(config));
+}
+
 } // namespace dunedaq::kafkaopmon
 
-
 DEFINE_DUNE_OPMON_FACILITY(dunedaq::kafkaopmon::streamOpMonFacility)
-
--- test/apps/opmon_publish_to_kafka_test.cxx
+++ test/apps/opmon_publish_to_kafka_test.cxx
@@ -3,8 +3,8 @@
  */
 
 #include <chrono>
+#include <future>
 #include <thread>
-#include <future>        
 
 #include "opmonlib/opmon/test.pb.h"
 
@@ -16,33 +16,33 @@ namespace po = boost::program_options;
 
 using namespace dunedaq::opmonlib;
 
+class TestObject : public MonitorableObject
+{
 
-class TestObject : public MonitorableObject {
-
-  public:
-    using MonitorableObject::register_node;
-    using MonitorableObject::publish;
-    TestObject() : MonitorableObject() {;}
+public:
+  using MonitorableObject::publish;
+  using MonitorableObject::register_node;
+  TestObject()
+    : MonitorableObject()
+  {
+    ;
+  }
 };
-
-
 
 int
 main(int argc, char const* argv[])
 {
   po::options_description desc("Allowed options");
-  desc.add_options()
-    ("help,h", "produce help message")
-    ("bootstrap", po::value<std::string>()->default_value("monkafka.cern.ch:30092"), "kafka bootstrap server")
-    ("topic,t", po::value<std::string>()->default_value("opmon_stream"), "Optional specification of a topic" )
-    ("n_threads,n", po::value<unsigned int>()->default_value(10), "Number of threads used for test") 
-  ;
+  desc.add_options()("help,h", "produce help message")(
+    "bootstrap", po::value<std::string>()->default_value("monkafka.cern.ch:30092"), "kafka bootstrap server")(
+    "topic,t", po::value<std::string>()->default_value("opmon_stream"), "Optional specification of a topic")(
+    "n_threads,n", po::value<unsigned int>()->default_value(10), "Number of threads used for test");
 
   po::variables_map input_map;
   po::store(po::parse_command_line(argc, argv, desc), input_map);
   po::notify(input_map);
-  
-  if ( input_map.count("help") ) {
+
+  if (input_map.count("help")) {
     std::cout << desc << std::endl;
     return 0;
   }
@@ -51,39 +51,36 @@ main(int argc, char const* argv[])
   uri += input_map["bootstrap"].as<std::string>();
   uri += '/';
   uri += input_map["topic"].as<std::string>();
-  
-  OpMonManager man( "test",
-		    "application",
-		    uri );
 
-  const auto n = input_map["n_threads"].as<unsigned int>() ;
+  OpMonManager man("test", "application", uri);
+
+  const auto n = input_map["n_threads"].as<unsigned int>();
   std::vector<std::shared_ptr<TestObject>> objs(n);
-  for ( size_t i = 0; i < n; ++i ) {
+  for (size_t i = 0; i < n; ++i) {
     auto p = objs[i] = std::make_shared<TestObject>();
-    man.register_node( "element_" + std::to_string(i), p );
+    man.register_node("element_" + std::to_string(i), p);
   }
 
-  auto pub_func = [&](int i, std::shared_ptr<TestObject> p){
-    
-    for (auto j = 0; j < 20; ++j ) {
+  auto pub_func = [&](int i, std::shared_ptr<TestObject> p) {
+    for (auto j = 0; j < 20; ++j) {
       dunedaq::opmon::TestInfo ti;
-      ti.set_int_example( j*1000 + i );
-      ti.set_string_example( "test" );
-      p->publish( std::move(ti) );
+      ti.set_int_example(j * 1000 + i);
+      ti.set_string_example("test");
+      p->publish(std::move(ti));
     }
   };
 
   std::vector<std::future<void>> threads(n);
-  
-  for( size_t i = 0 ; i < n; ++i ) {
+
+  for (size_t i = 0; i < n; ++i) {
     threads[i] = async(std::launch::async, pub_func, i, objs[i]);
   }
 
-  for ( auto & t : threads ) {
+  for (auto& t : threads) {
     t.get();
   }
-  
+
   std::this_thread::sleep_for(std::chrono::seconds(2));
-  
+
   return 0;
 }
